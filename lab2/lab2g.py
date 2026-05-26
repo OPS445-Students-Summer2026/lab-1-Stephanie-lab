c68782d898d5a76d1e19e7f8673d08ee0452c461
Stephanie Mmaduabuchi <simmaduabuchi@myseneca.ca>
2026-05-25 20:26:39 -0400

    Completed Lab 2

---
+++ lab2/lab2g.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Author: Stephanie MMaduabuchi
+# Author ID: 173830233
+# Date Created: 2026/05/25
+
+import sys
+
+if len(sys.argv) == 2:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
